9704a34c3c61aea0dd5620b61f18bc25b874a3ab
Alex Peattie <alexpeattie@gmail.com>
2013-10-15 14:00:24 +0100

    Add nested forms

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -11,7 +11,18 @@ class QuizzesController < ApplicationController
 
   def new
     @quiz = Quiz.new
-    @quiz.questions.build
+    @quiz.populate(num_of_questions: 5, num_of_answers: 4)
+  end
+
+  def create
+    @quiz = Quiz.new quiz_params
+
+    if @quiz.save
+      WebsocketRails[:quizzes].trigger 'new', @quiz
+      redirect_to @quiz
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -20,21 +31,20 @@ class QuizzesController < ApplicationController
 
   def update
     @quiz = Quiz.find params[:id]
-    if @quiz.update params[:quiz].permit(:title)
+    if @quiz.update quiz_params
       redirect_to @quiz
     else
       render 'edit'
     end
   end
 
-  def create
-    @quiz = Quiz.new params[:quiz].permit(:title, questions_attributes: [:query])
+  private
 
-    if @quiz.save
-      redirect_to @quiz
-    else
-      render 'new'
-    end
+  def quiz_params
+    params.require(:quiz)
+          .permit :title, questions_attributes: 
+                            [:id, :query, answers_attributes: 
+                                [:id, :response, :correctness]]
   end
 
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,8 +1,15 @@
 class Quiz < ActiveRecord::Base
 
   has_many :questions
-  accepts_nested_attributes_for :questions
+  accepts_nested_attributes_for :questions, reject_if: ->(q) { q[:query].blank? }
 
   validates :title, presence: true
+
+  def populate(num_of_questions: 0, num_of_answers: 0)
+    num_of_questions.times do
+      question = questions.build
+      question.populate_answers(num_of_answers)
+    end
+  end
 
 end
