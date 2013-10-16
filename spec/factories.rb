edd40c545bfabe8ab712867c75ed901a355523fd
Alex Peattie <alexpeattie@gmail.com>
2013-10-16 10:32:16 +0100

    Factorygirl

---
+++ spec/factories.rb
@@ -0,0 +1,20 @@
+FactoryGirl.define do
+  factory :quiz do
+    title "My quiz"
+  end
+
+  factory :right_answer, class: Answer do
+    sequence(:response) {|n| "Correct#{n}" }
+    correctness true
+  end
+
+  factory :wrong_answer, class: Answer do
+    sequence(:response) {|n| "Incorrect#{n}" }
+    correctness false
+  end
+
+  factory :question do
+    query "What is the right answer?"
+    answers { [create(:right_answer), create(:wrong_answer)] }
+  end
+end
